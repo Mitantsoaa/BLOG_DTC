fc5a27e9d4b5a410020a32c32ee3e3b2e5306108
Mitantsoaa <mitaranaivo@gmail.com>
2023-12-20 06:50:28 +0300

    User model

--- models/UserModel.php
+++ models/UserModel.php
@@ -3,14 +3,32 @@ require 'autoload.php';
 
     class UserModel
     {
-        public function listUser()
+        public function listUser($order = 'nom', $paginate, $start_from)
         {
+            try {
+                $pdo = DataBase::connect();
+                $sql = $pdo->prepare("SELECT * FROM users u GROUP BY u.id_user ORDER BY $order LIMIT $start_from, $paginate");
+                $sql->execute();
+                $result = $sql->fetchAll();
+                DataBase::disconnect();
+            } catch (PDOException  $e ){
+                echo "Error: ".$e;
+            }
 
+            return $result;
         }
 
-        public function addUser()
+        public function addUser($nom,$id_role)
         {
-
+            try {
+                $pdo = DataBase::connect();
+                $sql = $pdo->prepare("INSERT INTO users ( nom, id_role) VALUES ('".$nom."','".$id_role."')");
+                $sql->execute();
+                DataBase::disconnect();
+            } catch (Exception $e) {
+                DataBase::disconnect();
+                throw $e;
+            }
         }
 
         public function updateUser($id)
@@ -21,6 +39,22 @@ require 'autoload.php';
         public function removeUser($id)
         {
 
+        }
+
+        public function paginator ($limit)
+        {
+            try {
+                $pdo = DataBase::connect();
+                $sth = $pdo->prepare("SELECT COUNT(id_user) FROM users");
+                $sth->execute();
+                $result = $sth->fetchColumn();
+                DataBase::disconnect();
+                $total_pages = ceil($result / $limit);
+
+                return $total_pages;
+            } catch (PDOException  $e ){
+                echo "Error: ".$e;
+            }
         }
 
     }
