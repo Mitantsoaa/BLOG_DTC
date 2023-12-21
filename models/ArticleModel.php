7cd0535502c8204b05ba8950b065abefd411fc22
Mitantsoaa <mitaranaivo@gmail.com>
2023-12-21 06:54:59 +0300

    Articles list

--- models/ArticleModel.php
+++ models/ArticleModel.php
@@ -3,6 +3,17 @@
     {
         public function getAllArticles()
         {
+            try {
+                $pdo = DataBase::connect();
+                $sql = $pdo->prepare("SELECT * FROM articles a GROUP BY a.id ORDER BY $order LIMIT $start_from, $paginate");
+                $sql->execute();
+                $result = $sql->fetchAll();
+                DataBase::disconnect();
+            } catch (PDOException  $e ){
+                echo "Error: ".$e;
+            }
+
+            return $result;
 
         }
 
